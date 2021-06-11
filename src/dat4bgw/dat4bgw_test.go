4398a3b34a9324bd448c1de78d3bf3d08f86d34c
Vladimir Mironov <vladimir.n.mironov@gmail.com>
2021-06-11 15:11:07 +0200

    tdata/: pushed ouput RDF files

---
+++ src/dat4bgw/dat4bgw_test.go
@@ -0,0 +1,32 @@
+package main
+
+/*
+
+import (
+	"testing"
+)
+
+type tt struct {
+	arg1 string
+	arg2 string
+	arg3 [5]int
+	val1 error
+}
+
+func Test_x1tftg(t *testing.T) {
+	pth := "../../tdata/"
+	tts := []tt{
+		{pth + "test.f2g", pth + "htri.f2g", [5]int{2, 3, 9, 10, 0}, nil},
+	}
+	for i, tt := range tts {
+		err := x1tftg(tt.arg1, tt.arg2, tt.arg3)
+		if err != tt.val1 {
+			t.Error(
+				"For test", i+1, ": ",
+				"\n\twant", tt.val1,
+				"\n\thave", err,
+			)
+		}
+	}
+}
+*/
